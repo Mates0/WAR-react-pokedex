de1bf40c7f52f0ff6bc11060a66ae109ab41f991
Mates0 <matyas.vanko@educanet.cz>
2023-03-10 10:01:40 +0100

    Search done

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -2,20 +2,55 @@ import {useState, useEffect} from 'react';
 import styles from '../styles/Pokemonlist.module.css';
 
 function PokemonList() {
+    const [searchTerm, setSearchTerm] = useState("");
     const [pokemon, setPokemon] = useState([]);
+    const [filteredpokemon, setFilteredpokemon] = useState([]);
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=42')
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
             .then((response) => response.json())
             .then(({results}) => Promise.all(results.map(({url}) => fetch(url).then((response) => response.json()))))
-            .then((pokemonData) => setPokemon(pokemonData));
+            .then((pokemonData) => {
+                setPokemon(pokemonData)
+                setFilteredpokemon(pokemonData)
+            });
     }, []);
+
+    const handleSearchClick = () => {
+        console.log(searchTerm)
+        if (searchTerm !== "") {
+            const filtered = pokemon.filter((poke) =>
+                poke.name.includes(searchTerm.toLowerCase())
+            );
+            setFilteredpokemon(filtered)
+        } else {
+            setFilteredpokemon(pokemon)
+        }
+        setSearchTerm("")
+    };
+
+    const handleInputChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
 
     return (
         <div>
-            <h1 className={styles.title}>Pokemon List</h1>
+            <div>
+                <h1 className={styles.title}>Pokemon List</h1>
+                <div className={styles.inputcontainer}>
+                    <input
+                        type="text"
+                        placeholder="Search for a Pokemon"
+                        value={searchTerm}
+                        onChange={handleInputChange}
+                        className={styles.inputfield}
+                    />
+
+                    <button onClick={handleSearchClick} className={styles.searchbutton}>Search</button>
+                </div>
+            </div>
             <div className={styles.pokemonlist}>
-                {pokemon.map(({id, name, sprites, types, height, weight}) => (
+                {filteredpokemon.map(({id, name, sprites, types, height, weight}) => (
                     <div className={styles.pokemoncard} key={id}>
                         <img src={sprites.front_default} className={styles.image}/>
                         <h1 className={styles.name}>{name}</h1>
